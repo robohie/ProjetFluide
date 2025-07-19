aca1a4ef9df0337eedd4f10b260c8066b0738dc7
robohie <rogerbosolinndo34@gmail.com>
2025-07-19 23:56:42 +0100

    Amélioration du code

--- fluidflow.py
+++ fluidflow.py
@@ -20,6 +20,7 @@ import sympy
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def latex(objet):
     """Génère une section LaTeX résumant l'écoulement."""
     text = (
@@ -31,12 +32,14 @@ def latex(objet):
     )
     return text
 
-def courant(objet, a, b):
+
+def courant(objet, a, b, niveaux):
     """Trace les courbes de courant pour plusieurs niveaux."""
-    niveaux = [-3, -2, -1, 1, 2, 3]
     figures = []
     for n in niveaux:
-        fig = sympy.plot_implicit(objet.c_niv1(n), (objet.x, -a, a), (objet.y, -b, b), show=False, adaptive=False, depth=4)
+        fig = sympy.plot_implicit(objet.c_niv1(n), (objet.x, -a, a),
+                                  (objet.y, -b, b), show=False,
+                                  adaptive=False, depth=4)
         figures.append(fig)
     # Fusionner les figures
     fig_final = figures[0]
@@ -47,12 +50,13 @@ def courant(objet, a, b):
     fig_final.ylabel = "Y"
     return fig_final
 
-def potentiel(objet, a, b):
+
+def potentiel(objet, a, b, niveaux):
     """Trace les courbes de potentiel pour plusieurs niveaux."""
-    niveaux = [-3, -2, -1, 1, 2, 3]
     figures = []
     for n in niveaux:
-        fig = sympy.plot_implicit(objet.c_niv2(n), (objet.x, -a, a), (objet.y, -b, b), show=False)
+        fig = sympy.plot_implicit(objet.c_niv2(n), (objet.x, -a, a),
+                                  (objet.y, -b, b), show=False)
         figures.append(fig)
     fig_final = figures[0]
     for f in figures[1:]:
@@ -62,13 +66,14 @@ def potentiel(objet, a, b):
     fig_final.ylabel = "Y"
     return fig_final
 
+
 def champ(objet, x_lim=(-10, 10), y_lim=(-10, 10), density=20):
     """Affiche le champ vectoriel de vitesse."""
     X, Y = np.meshgrid(
         np.linspace(x_lim[0], x_lim[1], density),
         np.linspace(y_lim[0], y_lim[1], density)
     )
-    # Optimiser: compiler une seule fois les lambdas
+    #
     dphi_x_func = sympy.lambdify((objet.x, objet.y), objet.d_phi_x)
     dphi_y_func = sympy.lambdify((objet.x, objet.y), objet.d_phi_y)
     U = dphi_x_func(X, Y)
@@ -81,18 +86,22 @@ def champ(objet, x_lim=(-10, 10), y_lim=(-10, 10), density=20):
     plt.ylabel("Y")
     plt.show()
 
+
 class PlanPlus:
     """Classe de base pour les écoulements potentiels 2D."""
 
-    def __init__(self):
+    def __init__(self, nom: str = "PlanPlus"):
+        # Initialisation des dérivées partielles
         self.d_phi_y = None
         self.d_phi_x = None
+        # Initialisation de la fonction complèxe
         self.f = 0
+        # Déclaration des variables symboliques
         self.x = sympy.symbols("x", real=True)
         self.y = sympy.symbols("y", real=True)
         self.z = self.x + sympy.I * self.y
         self.t = sympy.symbols("t", real=True)
-        self.nom = "PlanPlus"
+        self.nom = nom
 
     def __add__(self, other):
         """Addition d'écoulements (potentiels complexes)."""
@@ -135,6 +144,7 @@ class PlanPlus:
     def __repr__(self):
         return self.__str__()
 
+
 class EUniforme(PlanPlus):
     """Ecoulement uniforme."""
 
@@ -148,31 +158,49 @@ class EUniforme(PlanPlus):
         self.nom = "uniforme"
         self.update()
 
-class Doublet(PlanPlus):
-    """Ecoulement doublet (source + puits)."""
+
+class Puits(PlanPlus):
+    """Cette classe implémente un puits
+    """
 
     def __init__(self, a: float = 1, qv: float = 1):
-        """
-        :param a: distance entre la source (ou le puits) et l'origine
-        :param qv: débit volumique
-        """
         super().__init__()
         self.a = a
         self.Qv = qv
-        # Sécurisation pour éviter division par zéro
-        if a == 0:
-            raise ValueError("La distance a ne peut être nulle pour un doublet.")
-        self.f = qv * sympy.log((self.z + a) / (self.z - a)) / (2 * sympy.pi)
-        self.nom = "doublet"
+        self.f = -qv * sympy.log(self.z - a) / (2 * sympy.pi)
+        self.nom = "puits"
         self.update()
 
-if __name__ == "__main__":
-    E1 = EUniforme(5)
-    E2 = Doublet(0.05, 0.5)
-    E = E1 + E2
 
-    fig1 = courant(E, 5, 5)
-    champ(E, (-0.5, 0.5), (-0.5, 0.5))
+class Source(PlanPlus):
+    """Cette classe implémente une
+    source
+    """
+
+    def __init__(self, a: float = 1, qv: float = 1):
+        super().__init__()
+        self.a = a
+        self.Qv = qv
+        self.f = qv * sympy.log(self.z + a) / (2 * sympy.pi)
+        self.nom = "source"
+        self.update()
+
+
+if __name__ == "__main__":
+    E1 = EUniforme(7)
+    E2 = Puits(1, 2)
+    E3 = Source(2, 2.5)
+
+    E4 = E3 + E2
+    E4.nom = "Doublet"
+    E = E1 + E4
+
+    fig1 = courant(E, 5, 5, (1, -1, -2, 2, 3, -3))
+    # fig2 = potentiel(E, 5, 5, (1, -1, -2, 2, 3, -3))
+    # champ(E4, (-5, 5), (-2, 2))
+
+    fig1.show()
+    # fig2.show()
     # Export LaTeX
     try:
         with open("ProjetMecaniqueFluide.tex", "w") as tex:
